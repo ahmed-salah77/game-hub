897b7fcf59198fb403460b40d3b7ee8b39e6a2fc
Ahmed <AhmedSalah9339@gmail.com>
2024-02-09 16:15:00 +0200

    delete game page background image

--- src/Pages/GameDetailPage.tsx
+++ src/Pages/GameDetailPage.tsx
@@ -1,12 +1,6 @@
 import { useParams } from "react-router-dom";
 import useGame from "../Entities/useGame";
-import {
-  Box,
-  GridItem,
-  Heading,
-  SimpleGrid,
-  Spinner,
-} from "@chakra-ui/react";
+import { Box, GridItem, Heading, SimpleGrid, Spinner } from "@chakra-ui/react";
 import ExpandableText from "../components/ExpandableText";
 import GameAttributes from "../components/gameAttributes";
 import GameTrailers from "../components/GameTrailers";
@@ -18,36 +12,17 @@ const GameDetailPage = () => {
   if (isLoading) return <Spinner />;
   if (!game) return null;
   return (
-    <>
-      <SimpleGrid columns={{ base: 1, md: 2 }}>
-        <GridItem>
-          <Heading>{game.name}</Heading>
-          <ExpandableText limit={400}>{game.description_raw}</ExpandableText>
-          <GameAttributes game={game} />
-        </GridItem>
-        <GridItem>
-          <GameTrailers id={game.id} />
-          <GameScreenshots id={game.id} />
-        </GridItem>
-      </SimpleGrid>
-      <Box
-        position={"absolute"}
-        left={0}
-        top={0}
-        zIndex="-2"
-        h="100%"
-        w="100%"
-        bg={"#151515"}
-      >
-        <Box
-          h="600px"
-          backgroundColor="transparent"
-          backgroundSize="cover"
-          backgroundImage={`linear-gradient(rgba(15, 15, 15, 0), rgb(21, 21, 21)), linear-gradient(rgba(21, 21, 21, 0.8), rgba(21, 21, 21, 0.5)), url(${game.background_image})`}
-          zIndex="1"
-        ></Box>
-      </Box>
-    </>
+    <SimpleGrid columns={{ base: 1, md: 2 }}>
+      <GridItem>
+        <Heading>{game.name}</Heading>
+        <ExpandableText limit={400}>{game.description_raw}</ExpandableText>
+        <GameAttributes game={game} />
+      </GridItem>
+      <GridItem>
+        <GameTrailers id={game.id} />
+        <GameScreenshots id={game.id} />
+      </GridItem>
+    </SimpleGrid>
   );
 };
 
